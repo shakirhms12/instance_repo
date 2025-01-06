cae7447c9072cef7f04ae49677ff25c4cec5950a
shakirhms12 <2020a1r019@mietjammu.in>
2025-01-06 10:22:54 +0530

    1

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region     = "us-east-1" 
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "ec2_example_01" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  # subnet_id     = aws_subnet.subnet_aws.id
+  # security_groups = [aws_security_group.security_group.id]
+  key_name      = var.key_name
+
+  tags = {
+    Name = var.tags
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
+
---
+++ variable.tf
@@ -0,0 +1,40 @@
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  default = "ami-0e2c8caa4b6378d8c"
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  default     = "t2.micro"
+}
+
+variable "tags" {
+  description = "Name of the EC2 instance"
+  default     = "my-ec2-01"
+}
+
+# variable "cidr_vpc" {
+#   description = "CIDR Block value"
+#   default = "10.0.0.0/16"
+# }
+
+# variable "cidr_subnet" {
+#   description = "CIDR Block value"
+#   default = "10.0.1.0/24"
+# }
+
+# variable "availability_zone" {
+#   default = "us-east-1a"
+# }
+
+variable "key_name" {
+  default = "my_aws_key"
+}
+
+variable "access_key" {
+  description = "access key"
+}
+
+variable "secret_key" {
+  description = "secret key"
+}
